9099d95cde2390c44aaba55b5ad0c22bb4d849fa
Gabriel <gabrielmachado.emp@gmail.com>
2022-08-19 20:33:56 -0300

    Solução do Error

--- agenda/settings.py
+++ agenda/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contactux',
     'contatos.apps.ContatosConfig',
     'accounts.apps.AccountsConfig',
 ]
